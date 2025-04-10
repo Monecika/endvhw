42bdbf8bf082a2d83f200beedb8899196a473396
Monecika <opreaconstatnin1990@gmail.com>
2025-04-10 19:15:11 +0300

    first commit

---
+++ src/main/java/ceiti/md/App.java
@@ -0,0 +1,13 @@
+package ceiti.md;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
